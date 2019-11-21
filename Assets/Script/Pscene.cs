92ef5c7d8fbde3f7f8af7f29a3bf00dcbd64de95
Azakiindra <40498333+Azakiindra@users.noreply.github.com>
2019-11-21 23:02:48 +0700

    Add files via upload

---
+++ Assets/Script/Pscene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pscene : MonoBehaviour {
+
+    public string Bname;
+    public void TButtonboy(Button btn)
+    {
+        Debug.Log(btn.name);
+        Bname = btn.name;
+    }
+    public void Cscene()
+    {
+        Scene cekcoy = SceneManager.GetActiveScene();
+        if(cekcoy.name == "MainMenu")
+        {
+            if(Bname == "BPlay")
+            {
+                SceneManager.LoadScene("Play");
+            }
+            else if(Bname == "BAbout")
+            {
+                SceneManager.LoadScene("About");
+            }
+            else if (Bname == "BExit")
+            {
+                Application.Quit();
+            }
+        }
+        if(cekcoy.name == "About" && Bname == "ABKembali")
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        if(cekcoy.name == "Play")
+        {
+            if(Bname == "ToMainMenu")
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/SoalM.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoalM : MonoBehaviour {
+
+    [System.Serializable]
+    public class soal
+    {
+        [TextArea]
+        [Header("soal")]
+
+        public string inisoal;
+
+
+        [Header("pilihan")]
+        public string iniA;
+        public string iniB, iniC, iniD;
+
+
+        [Header("jawaban")]
+        public bool A;
+        public bool B, C, D;
+
+    }
+
+    Text teksoal, teksA, teksB, teksC, teksD;
+
+    public List<soal> soalnya;
+    // Use this for initialization
+    void Start()
+    {
+        teksoal = GameObject.Find("Tsoal").GetComponent<Text>();
+        teksA = GameObject.Find("A").GetComponent<Text>();
+        teksB = GameObject.Find("B").GetComponent<Text>();
+        teksC = GameObject.Find("C").GetComponent<Text>();
+        teksD = GameObject.Find("D").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        teksoal.text = soalnya[0].inisoal;
+        teksA.text = soalnya[0].iniA;
+        teksB.text = soalnya[0].iniB;
+        teksC.text = soalnya[0].iniC;
+        teksD.text = soalnya[0].iniD;
+    }
+}
